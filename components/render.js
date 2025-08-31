ba83140eaec858ffaba6451a1e4bd83e3a58e680
Bills-6 <conutconut6@gmail.com>
2025-08-31 21:19:36 +0700

    add class tailwind array

--- components/render.js
+++ components/render.js
@@ -11,7 +11,13 @@ const styling = {
 
 	bodyTodo: ['body-todo', 'py-1', 'flex', 'items-center', 'flex-col', 'lg:flex-row gap-1'],
 	todoActivityWrapper: ['todo-input', 'resize-none', 'block', 'w-full', 'md:text-lg', 'lg:text-[1.5rem]', 'font-semibold'],
-	todoInput: ['todo-input', 'resize-none', 'block', 'w-full', 'md:text-lg', 'lg:text-[1.5rem]', 'font-semibold']
+	todoInput: ['todo-input', 'resize-none', 'block', 'w-full', 'md:text-lg', 'lg:text-[1.5rem]', 'font-semibold'],
+	editTodoButton: ['edit-todo-button', 'w-[30px]', 'h-[30px]', 'lg:w-[40px]', 'lg:h-[40px]', 'absolute', 'right-[10px]', 'top-[50%]', 'translate-y-[-50%]', 'flex', 'items-center', 'justify-center', 'opacity-0', 'hover:cursor-pointer', 'hover:scale-[1.1]'],
+	iconEditTodo: ['material-symbols-outlined', 'size', 'pointer-events-none'],
+
+	todoCheckboxWrapper: ['todo-checkbox-wrapper', 'w-full', 'lg:w-fit', 'py-1', 'flex', 'gap-3', 'justify-center'],
+	inputCheckboxWrapper: ['input-checkbox-wrapper', 'w-[30px]', 'h-[30px]', 'md:w-[40px]', 'md:h-[40px]', 'lg:w-[50px]', 'lg:h-[50px]', 'relative'],
+	inputCheckbox: ['block', 'w-full', 'h-full']
 }
 
 const ul = document.getElementById("list-wrapper");
