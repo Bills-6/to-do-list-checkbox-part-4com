d95eafd916e9bc28647c4238fe84116fc5960bbe
Bills-6 <conutconut6@gmail.com>
2025-09-03 00:31:54 +0700

    update: filter panel open/close script

--- components/filter.js
+++ components/filter.js
@@ -1,1 +1,30 @@
 const filterButton = document.getElementById("filter-button");
+const filterPenel = document.getElementById("filter-panel");
+
+filterButton.addEventListener("click", function() {
+	if (filterPenel.dataset.status === "close") {
+		filterPanelVisibility("open");
+	} else filterPanelVisibility("close");
+});
+
+function filterPanelVisibility(forWhat) {
+	if (forWhat === "open") {
+		filterPenel.dataset.status = "open";
+		filterPenel.classList.remove("hidden");
+	} else {
+		filterPenel.dataset.status = "close";
+		filterPenel.classList.add("hidden");
+	}
+}
+
+document.addEventListener("click", (e) => {
+	if (!filterPenel.contains(e.target) &&
+		!filterButton.contains(e.target)
+	) {
+		filterPanelVisibility("close");
+	}
+});
+
+document.addEventListener("keydown", (is) => {
+	if (is.key === "Escape") filterPanelVisibility("close");
+});
